16d373b3e168fc291cda31f0191d7e7e59a1c7f8
TheRohitsardar <110388039+TheRohitsardar@users.noreply.github.com>
2024-03-03 09:24:26 +0530

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text", {
+    Strings: ["Tester" , "Frontend Developer" , "Quality Analyst"],
+    typedSpeed: 100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+});
